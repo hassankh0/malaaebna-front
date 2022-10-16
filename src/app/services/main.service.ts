301c9b7fc75b312f3a81938429a3b98774b7b65d
hassankh0 <hassan.h.khalife@outlook.com>
2022-10-16 20:58:00 +0200

    reservation

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -16,47 +16,47 @@ export class MainService {
   constructor(private http: HttpClient) { }
 
   public fetchTerrains() {
-    // this.terrains = [
-    //   new Terrain("1", "terrain-1", 10, "Calais","../../../assets/foot.png"),
-    //   new Terrain("2", "terrain-2", 15, "Boulogne","../../../assets/foot.png"),
-    //   new Terrain("3", "terrain-3", 20, "Lille", "../../../assets/foot.png"),
-    //   new Terrain("4", "terrain-4", 20, "Lille", "../../../assets/foot.png"),
-    //   new Terrain("5", "terrain-5", 20, "Lille", "../../../assets/foot.png")
-    // ];
-    const baseURL = 'http://localhost:1337';
-    const qs = require('qs');
-    const query = qs.stringify({
-      populate: '*',
-    }, {
-      encodeValuesOnly: true, // prettify URL
-    });
-    this.http.get(`/api/terrains?${query}`)
-      .subscribe((data: any) => {
-        console.log(data)
-        data.data.map((da: any) => {
-          let d = da.attributes;
-        console.log(d);
-          let terrain: Terrain = new Terrain(d.idTerrain, d.nom, d.prix, d.adresse,`${baseURL}${d.photo.data.attributes.url}`);
-          this.terrains.push(terrain);
-        });
-      });
+    this.terrains = [
+      new Terrain("1", "terrain-1", 10, "Calais","../../../assets/foot.png"),
+      new Terrain("2", "terrain-2", 15, "Boulogne","../../../assets/foot.png"),
+      new Terrain("3", "terrain-3", 20, "Lille", "../../../assets/foot.png"),
+      new Terrain("4", "terrain-4", 20, "Lille", "../../../assets/foot.png"),
+      new Terrain("5", "terrain-5", 20, "Lille", "../../../assets/foot.png")
+    ];
+    // const baseURL = 'http://localhost:1337';
+    // const qs = require('qs');
+    // const query = qs.stringify({
+    //   populate: '*',
+    // }, {
+    //   encodeValuesOnly: true, // prettify URL
+    // });
+    // this.http.get(`/api/terrains?${query}`)
+    //   .subscribe((data: any) => {
+    //     console.log(data)
+    //     data.data.map((da: any) => {
+    //       let d = da.attributes;
+    //     console.log(d);
+    //       let terrain: Terrain = new Terrain(d.idTerrain, d.nom, d.prix, d.adresse,`${baseURL}${d.photo.data.attributes.url}`);
+    //       this.terrains.push(terrain);
+    //     });
+    //   });
     console.log(this.terrains);
   }
 
   public fetchReservations() {
-    // this.reservations = [
-    //   new Reservation("1", "1", "hassan", "0613172014", new Date('2022-10-12T14:00:00'), new Date('2022-10-12T15:00:00')),
-    //   new Reservation("2", "2", "atwe", "0613172014", new Date('2022-10-12T14:00:00'), new Date('2022-10-12T15:00:00'))
-    // ];
-    this.http.get('/api/reservations')
-      .subscribe((data: any) => {
-        data.data.map((da: any) => {
-          let d = da.attributes;
-          console.log(d);
-          let reservation: Reservation = new Reservation(d.idReservation, d.terrainId, d.nomPers, d.phone, new Date(d.startTime), new Date(d.endTime));// new  (d.idTerrain, d.nom, d.prix, d.adresse, "../../../assets/foot.png");
-          this.reservations.push(reservation);
-        });
-      });
+    this.reservations = [
+      new Reservation("1", "1", "hassan", "0613172014", new Date('2022-10-12T14:00:00'), new Date('2022-10-12T15:00:00')),
+      new Reservation("2", "2", "atwe", "0613172014", new Date('2022-10-12T14:00:00'), new Date('2022-10-12T15:00:00'))
+    ];
+    // this.http.get('/api/reservations')
+    //   .subscribe((data: any) => {
+    //     data.data.map((da: any) => {
+    //       let d = da.attributes;
+    //       console.log(d);
+    //       let reservation: Reservation = new Reservation(d.idReservation, d.terrainId, d.nomPers, d.phone, new Date(d.startTime), new Date(d.endTime));// new  (d.idTerrain, d.nom, d.prix, d.adresse, "../../../assets/foot.png");
+    //       this.reservations.push(reservation);
+    //     });
+    //   });
       console.log(this.reservations);
   }
   public getTitle():String {
